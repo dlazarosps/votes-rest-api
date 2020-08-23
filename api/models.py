5f11720f2372862c9596eb6c914981e7ef3c7233
dlazarosps <dlspsilva@inf.ufrgs.br>
2020-08-22 23:46:42 -0300

    [ADD] model agenda & migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Agenda(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+
+    def __str__(self):
+        return self.title
+    
