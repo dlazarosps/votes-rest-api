1f2d07a06e21e8ca38781fc0a7fbfa88e02bea30
dlazarosps <dlspsilva@inf.ufrgs.br>
2020-08-23 21:46:15 -0300

    [UPDATE] urls static assets

--- votes_rest_api/urls.py
+++ votes_rest_api/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 from api.api import AgendaViewSet, SessionViewSet, UserViewSet, VoteViewSet
 
@@ -27,4 +29,4 @@ router.register(r'votes', VoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v0/', include(router.urls))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
